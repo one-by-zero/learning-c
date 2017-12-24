13310e2070c50b808bfcbb54b0fe932677cdea3c
Sandhya Neupane <neupanesandyy@gmail.com>
2017-12-25 01:03:15 +0545

    no square

---
+++ number-square.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int n;
+  printf("Enter a number.");
+ 
+  scanf("%d", &n);
+  printf("Hello!! you entered the number %d\n and it's square is %d\n", n, n*n);
+  
+  return 0;
+  }
+
+
+
+
+
